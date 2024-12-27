f360e3fbb1e6b3c2105bfb319da954ad3de3a353
TerlanAliyev <164336124+TerlanAliyev@users.noreply.github.com>
2024-12-27 23:04:08 +0400

    newspaper problem

--- Admindash-bab72e549c9438be7dbf93deed957e50db05d142/Argon/obj/Release/net8.0/Argon.AssemblyInfo.cs
+++ Admindash-bab72e549c9438be7dbf93deed957e50db05d142/Argon/obj/Release/net8.0/Argon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Argon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dc38a08bdbec3b21a920b9b5be194f00e67eccd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3200b1a01bfb8b308514d1fdf5137358437d00a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Argon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Argon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
